bd298726a6f7f2734a06a39e92c847d7b98b9747
Michael van Meerwijk <michaelvanm@gmail.com>
2018-03-21 17:23:48 +0100

    added style colors for session types

--- src/App.vue
+++ src/App.vue
@@ -107,6 +107,12 @@ html {
 
 /* GRADIENTS https://webgradients.com/ */
 
+:root {
+	--focus: hsl(208,79,55);
+	--short: hsl(174,50,51);
+	--long: hsl(122,50,60);
+}
+
 .theme-dark {
 	background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; 
  	background-blend-mode: multiply,multiply;
